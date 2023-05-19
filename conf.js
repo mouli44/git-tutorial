134ceb1b8840f98cd950889c59cb01d9a54ddf04
Moule_VJ <moulivj4@gmail.com>
2023-05-19 22:26:45 +0530

    Version_1 restored

--- conf.js
+++ conf.js
@@ -2,17 +2,5 @@
 
 console.log("Config File !");
 console.log("Config Files Implemented ! !");
-console.log("Version 1 initialized");
 
-// Version 2
-
-console.log("Version 2");
-console.log("Version 2 Implemented !");
-console.log("Version 2 Initialized  !");
-
-
-// Version 3
-
-console.log("Version 3");
-console.log("Version 3 Implemented !");
-console.log("Version 3 Initialized  !");
+console.log("Version 1 initialized")
--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@
 
 console.log("Index File !");
 console.log("Index File with version 1");
+
 console.log("Version 1 initialized");
 
-// Version 2
 
-console.log("Version 2");
-console.log("Version 2 Implemented !");
-console.log("Version 2 Initialized  !");
-
-// Version 3
-
-console.log("Version 3");
-console.log("Version 3 Implemented !");
-console.log("Version 3 Initialized  !");
