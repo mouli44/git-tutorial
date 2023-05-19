3a5a1f399e1bea7ae07496c16a8830661c402f23
Moule_VJ <moulivj4@gmail.com>
2023-05-19 22:34:07 +0530

    Version_3 restored

--- src/change.js
+++ src/change.js
@@ -3,4 +3,14 @@
 console.log("change File !");
 console.log("change file is Implemented !");
 
-// 
+// Version 2
+
+console.log("Version 2");
+console.log("Version 2 Implemented !");
+console.log("Version 2 Initialized  !");
+
+// Version 3
+
+console.log("Version 3");
+console.log("Version 3 Implemented !");
+console.log("Version 3 Initialized  !");
--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@
 
 console.log("Main File !");
 console.log("Main File with Version 1");
-
 console.log("Version 1 initialized");
+
+
+// Version 2
+
+console.log("Version 2");
+console.log("Version 2 Implemented !");
+console.log("Version 2 Initialized  !");
+
+
+// Version 3
+
+console.log("Version 3");
+console.log("Version 3 Implemented !");
+console.log("Version 3 Initialized  !");
